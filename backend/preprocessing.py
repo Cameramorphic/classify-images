ae6f3189ad054915a51d6f58895f3f2980b2fe0e
Tobsikrid <mr.tobias.s@web.de>
2021-02-02 19:49:48 +0100

    add multipart response by video_retrieval

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -1,3 +1,5 @@
+from flask import Response
+
 import app
 
 import torch
@@ -14,6 +16,7 @@ model, preprocess = clip.load("ViT-B/32", device=device)
 
 image_mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device)
 image_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device)
+
 
 def predict_multiple():
     dir = app.app.config['UPLOAD_FOLDER']
@@ -39,13 +42,14 @@ def predict_multiple():
         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
 
     dic = {imagenames[i]: texts[np.argmax(probs[i])] for i in range(len(probs))}
-    #results = ['<tr><td>' + key + ': </td><td>' + dic[key] + '</td></tr>' for key in dic]
-    #m = np.argmax(probs, axis=-1)
-    #res = [imagenames[i] + " :  " + texts[i] for i in m]
-    #return '<table>' + ' '.join(results) + '</table>'
+    # results = ['<tr><td>' + key + ': </td><td>' + dic[key] + '</td></tr>' for key in dic]
+    # m = np.argmax(probs, axis=-1)
+    # res = [imagenames[i] + " :  " + texts[i] for i in m]
+    # return '<table>' + ' '.join(results) + '</table>'
     return json.dumps(dic)
 
-#currently only one video at a time (but with multiple textual descriptions)
+
+# currently only one video at a time (but with multiple textual descriptions)
 def video_retrieval():
     dir = app.app.config['UPLOAD_FOLDER']
     texts = []
@@ -76,42 +80,48 @@ def video_retrieval():
     for i in range(len(texts)):
         filepaths.append(saveImageFromVideo(videopath, np.argmax(probs[i])))
 
+    text_file_time = {}
+    for i in range(len(texts)):
+        text_file_time[texts[i]] = {filepaths[i]: str(np.argmax(probs[i]))}
+
+    name = os.path.join(app.app.config['UPLOAD_FOLDER'], 'data.json')
+
+    print(filepaths)
 
 
-    testpath = filepaths[0]
-    testfile_base = os.path.basename(testpath)
+    with open(name, 'w', encoding='utf-8') as f:
+        json.dump(text_file_time, f, ensure_ascii=False, indent=4)
+        filepaths.append(name)
     m = MultipartEncoder(
-           fields={'field1': 'value', 'field1': 'value',
-                   'image': (testfile_base, open(testpath, 'rb'), 'image/jpg')}
-        )
-    #return Response(m.to_string(), mimetype=m.content_type)
+        fields={
+            fname: open(fname, 'rb') for fname in filepaths}
+    )
 
-    results = ['<tr><td>' + texts[i] + ': </td><td>' + str(np.argmax(probs[i])) + '</td></tr>' for i in range(len(texts))]
-    return '<table>' + ' '.join(results) + '</table>'
-
+    return Response(m.to_string(), mimetype=m.content_type)
+    #results = ['<tr><td>' + key + ': </td><td>' + text_prob[key] + '</td></tr>' for key in text_prob]
+    #return '<table>' + ' '.join(results) + '</table>'
 
 
 def extractImages(pathIn):
     images = []
     count = 0
     vidcap = cv2.VideoCapture(pathIn)
-    success,image = vidcap.read()
+    success, image = vidcap.read()
     success = True
     while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
-        success,image = vidcap.read()
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 1000))
+        success, image = vidcap.read()
         if success:
             images.append(Image.fromarray(image))
             count = count + 1
     return images, count
 
-#index in seconds
+
+# index in seconds
 def saveImageFromVideo(pathIn, index):
     vidcap = cv2.VideoCapture(pathIn)
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,(index*1000))
-    success,image = vidcap.read()
+    vidcap.set(cv2.CAP_PROP_POS_MSEC, (index * 1000))
+    success, image = vidcap.read()
     image_path = os.path.basename(pathIn) + '-' + str(index) + '.jpg'
     cv2.imwrite(image_path, image)
     return image_path
-
-
