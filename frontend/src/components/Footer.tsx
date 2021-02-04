b945d8364965da8711edbb76dc56010f8f0b3246
Cameramorphic <anamorphicmirror@gmail.com>
2021-02-04 18:39:58 +0100

    fixed horizontal scrolling bug

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import clx from 'classnames';
-
 import { containerClass } from 'App';
 
 import styles from './Footer.module.css';
 
 export default function Footer() {
     return (
-        <div className={clx(styles.footer, containerClass)}>
-            <div className={styles.copyright}>
-                Copyright &copy; {new Date().getFullYear()} ClassifyImages GmbH
+        <div className={containerClass}>
+            <div className={styles.footer}>
+                <div className={styles.copyright}>
+                    Copyright &copy; {new Date().getFullYear()} ClassifyImages GmbH
+                </div>
             </div>
         </div>
     );
