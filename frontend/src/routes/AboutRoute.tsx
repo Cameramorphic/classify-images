71e13edff93a57ee2f82e0b962131ee41a17179d
Cameramorphic <anamorphicmirror@gmail.com>
2021-02-04 15:10:46 +0100

    converted AboutRoute to functional component

--- frontend/src/routes/AboutRoute.tsx
+++ frontend/src/routes/AboutRoute.tsx
@@ -1,50 +1,21 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ReactMarkdown from 'react-markdown'
 
-import { Form } from 'rsuite';
+import readme from './README.md';
 
 import styles from './AboutRoute.module.css';
 
-import readme from './README.md';
+export default function AboutRoute() {
+  const [mdText, setMdText] = useState('');
 
-type textState = {
-    mdtext: string
+  useEffect(() => {
+    fetch(readme).then(response => response.text()).then(text => setMdText(text));
+  }, []);
+
+  return (
+    <div className={styles.mdContainer}>
+      <ReactMarkdown source={mdText} />
+    </div>
+  );
 }
-
-export default class AboutRoute extends Component<{}, textState> {
-    constructor() {
-        super({});
-        this.state = { mdtext: "set in constructor" }; //used setState here did not work
-    }
-
-    componentWillMount() {
-      fetch(readme).then((response) => response.text()).then((text) => {
-        this.setState({ mdtext: text });
-      })
-    }
-
-    render() {
-      return (
-        <div>
-            <Form className={styles.form}>
-                <ReactMarkdown source={this.state.mdtext} />
-            </Form>
-        </div>
-      )
-    }
-}
-
-// export default function AboutRoute() {
-//     var filetext: string;
-//     fetch(readme).then((response) => response.text()).then((text) => {
-//         filetext = text;
-//         return (
-//             <div>
-//                 <Form className={styles.form}>
-//                     <ReactMarkdown source={filetext} />
-//                 </Form>
-//             </div>
-//         );
-//     })
-// }
